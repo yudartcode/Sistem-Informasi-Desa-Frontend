02a863258a5be3792e691bea062ea97ad081c565
yudartcode <yuda.karnaen11@gmail.com>
2020-09-02 11:34:36 +0800

    init

---
+++ src/views/dashboard/components/core/Drawer.vue
@@ -0,0 +1,270 @@
+<template>
+  <v-navigation-drawer
+    id="core-navigation-drawer"
+    v-model="drawer"
+    :dark="barColor !== 'rgba(228, 226, 226, 1), rgba(255, 255, 255, 0.7)'"
+    :expand-on-hover="expandOnHover"
+    :right="$vuetify.rtl"
+    :src="barImage"
+    mobile-break-point="960"
+    app
+    width="260"
+    v-bind="$attrs"
+  >
+    <template v-slot:img="props">
+      <v-img
+        :gradient="`to bottom, ${barColor}`"
+        v-bind="props"
+      />
+    </template>
+
+    <v-list
+      dense
+      nav
+    >
+      <v-list-item>
+        <v-list-item-avatar
+          class="align-self-center"
+          color="white"
+          contain
+        >
+          <v-img
+            src="https://demos.creative-tim.com/vuetify-material-dashboard/favicon.ico"
+            max-height="30"
+          />
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title
+            class="display-1"
+            v-text="profile.title"
+          />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider class="mb-2" />
+
+    <v-list
+      expand
+      nav
+    >
+      <!-- Style cascading bug  -->
+      <!-- https://github.com/vuetifyjs/vuetify/pull/8574 -->
+      <div />
+
+      <template v-for="(item, i) in computedItems">
+        <base-item-group
+          v-if="item.children"
+          :key="`group-${i}`"
+          :item="item"
+        >
+          <!--  -->
+        </base-item-group>
+
+        <base-item
+          v-else
+          :key="`item-${i}`"
+          :item="item"
+        />
+      </template>
+
+      <!-- Style cascading bug  -->
+      <!-- https://github.com/vuetifyjs/vuetify/pull/8574 -->
+      <div />
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  // Utilities
+  import {
+    mapState,
+  } from 'vuex'
+
+  export default {
+    name: 'DashboardCoreDrawer',
+
+    props: {
+      expandOnHover: {
+        type: Boolean,
+        default: false,
+      },
+    },
+
+    data: () => ({
+      items: [
+        {
+          icon: 'mdi-view-dashboard',
+          title: 'Dashboard',
+          to: '/',
+        },
+        {
+          icon: 'mdi-file-document-multiple',
+          title: 'Info Desa',
+          to: '/pages/user',
+          children: [
+            {
+              icon: 'mdi-view-dashboard',
+              title: 'Penduduk',
+              to: '',
+            },
+            {
+              icon: '',
+              title: 'Sarana Penunjang',
+              to: '',
+            },
+            {
+              icon: '',
+              title: 'Perangkat Desa',
+              to: '',
+            },
+            {
+              icon: '',
+              title: 'Administrasi',
+              to: '',
+            },
+            {
+              icon: '',
+              title: 'Umum',
+              to: '',
+            },
+            {
+              icon: '',
+              title: 'Potensi Desa',
+              to: '',
+            },
+            {
+              icon: '',
+              title: 'Produk Unggulan',
+              to: '',
+            }],
+        },
+        {
+          title: 'Kependudukan',
+          icon: 'mdi-account-group',
+          to: '/tables/regular-tables',
+          children: [
+            {
+              icon: 'mdi-home-heart',
+              title: 'Kartu Keluarga',
+              to: '',
+            },
+            {
+              icon: '',
+              title: 'Penduduk',
+              to: '',
+            }],
+        },
+        {
+          title: 'Struktur Organisasi',
+          icon: 'mdi-sitemap',
+          to: '/components/typography',
+        },
+        {
+          title: 'Visi Misi',
+          icon: 'mdi-chart-bubble',
+          to: '/components/icons',
+        },
+        {
+          title: 'Letak Geografis',
+          icon: 'mdi-google-maps',
+          to: '/maps/google-maps',
+        },
+        {
+          title: 'Program Desa',
+          icon: 'mdi-briefcase',
+          to: '/components/notifications',
+        },
+      ],
+    }),
+
+    computed: {
+      ...mapState(['barColor', 'barImage']),
+      drawer: {
+        get () {
+          return this.$store.state.drawer
+        },
+        set (val) {
+          this.$store.commit('SET_DRAWER', val)
+        },
+      },
+      computedItems () {
+        return this.items.map(this.mapItem)
+      },
+      profile () {
+        return {
+          avatar: true,
+          title: this.$t('app-name'),
+        }
+      },
+    },
+
+    methods: {
+      mapItem (item) {
+        return {
+          ...item,
+          children: item.children ? item.children.map(this.mapItem) : undefined,
+          title: this.$t(item.title),
+        }
+      },
+    },
+  }
+</script>
+
+<style lang="sass">
+  @import '~vuetify/src/styles/tools/_rtl.sass'
+
+  #core-navigation-drawer
+    .v-list-group__header.v-list-item--active:before
+      opacity: .24
+
+    .v-list-item
+      &__icon--text,
+      &__icon:first-child
+        justify-content: center
+        text-align: center
+        width: 20px
+
+        +ltr()
+          margin-right: 24px
+          margin-left: 12px !important
+
+        +rtl()
+          margin-left: 24px
+          margin-right: 12px !important
+
+    .v-list--dense
+      .v-list-item
+        &__icon--text,
+        &__icon:first-child
+          margin-top: 10px
+
+    .v-list-group--sub-group
+      .v-list-item
+        +ltr()
+          padding-left: 8px
+
+        +rtl()
+          padding-right: 8px
+
+      .v-list-group__header
+        +ltr()
+          padding-right: 0
+
+        +rtl()
+          padding-right: 0
+
+        .v-list-item__icon--text
+          margin-top: 19px
+          order: 0
+
+        .v-list-group__header__prepend-icon
+          order: 2
+
+          +ltr()
+            margin-right: 8px
+
+          +rtl()
+            margin-left: 8px
+</style>
