0a22c9527569b508d5f48d6f30c70adb316bcfef
yudartcode <yuda.karnaen11@gmail.com>
2020-09-11 13:12:35 +0800

    perbaiki dikit

--- src/views/dashboard/kependudukan/Listpenduduk.vue
+++ src/views/dashboard/kependudukan/Listpenduduk.vue
@@ -40,56 +40,6 @@
               $36,738
             </td>
           </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Minverva Hooper</td>
-            <td>Curaçao</td>
-            <td>Sinaas-Waas</td>
-            <td class="text-right">
-              $23,789
-            </td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Sage Rodriguez</td>
-            <td>Netherlands</td>
-            <td>Baileux</td>
-            <td class="text-right">
-              $56,142
-            </td>
-          </tr>
-
-          <tr>
-            <td>4</td>
-            <td>Philip Chaney</td>
-            <td>Korea, South</td>
-            <td>Overland Park</td>
-            <td class="text-right">
-              $38,735
-            </td>
-          </tr>
-
-          <tr>
-            <td>5</td>
-            <td>Doris Greene</td>
-            <td>Malawi</td>
-            <td>Feldkirchen in Kärnten</td>
-            <td class="text-right">
-              $63,542
-            </td>
-          </tr>
-
-          <tr>
-            <td>6</td>
-            <td>Mason Porter</td>
-            <td>Chile</td>
-            <td>Gloucester</td>
-            <td class="text-right">
-              $78,615
-            </td>
-          </tr>
         </tbody>
       </v-simple-table>
     </base-material-card>
@@ -97,7 +47,7 @@
 </template>
 <script>
   export default {
-    name: 'DashboardSarana',
+    name: 'ListPenduduk',
 
     data: () => ({
     }),
