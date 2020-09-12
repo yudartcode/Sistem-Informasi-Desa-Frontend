109202f84e25512a2b0d80aabdd12c88a1815a21
yudartcode <yuda.karnaen11@gmail.com>
2020-09-12 19:51:58 +0800

    Fix Login Issue

--- src/views/dashboard/pages/Login.vue
+++ src/views/dashboard/pages/Login.vue
@@ -1,57 +1,58 @@
 <template>
-  <v-container
-    id="kk"
-    fluid
-    tag="section"
-  >
-    <v-row
-      justify="center"
-    >
-      <v-col
-        cols="12"
-        sm="8"
-        md="4"
+  <v-app id="login">
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
       >
-        <base-material-card
-          icon="mdi-account"
-          class="px-5 py-3"
-          title="Login"
+        <v-row
+          align="center"
+          justify="center"
         >
-          <v-card-text>
-            <v-form>
-              <v-text-field
-                label="Login"
-                name="login"
-                prepend-icon="mdi-account"
-                type="text"
-              />
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <base-material-card
+              icon="mdi-account"
+              class="px-5 py-3"
+              title="Login"
+            >
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  />
 
-              <v-text-field
-                id="password"
-                label="Password"
-                name="password"
-                prepend-icon="mdi-lock"
-                type="password"
-              />
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn color="primary">
-              Login
-            </v-btn>
-          </v-card-actions>
-        </base-material-card>
-      </v-col>
-    </v-row>
-  </v-container>
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">
+                  Login
+                </v-btn>
+              </v-card-actions>
+            </base-material-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-  export default {
-    name: 'DashboardPenduduk',
-
-    data: () => ({
-    }),
-  }
+  export default {}
 </script>
+
+<style>
