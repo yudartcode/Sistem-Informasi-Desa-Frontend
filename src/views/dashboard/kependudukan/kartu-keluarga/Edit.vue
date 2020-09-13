db019531b43a44b73f7e6012081e9786c3fc313b
yudartcode <yuda.karnaen11@gmail.com>
2020-09-13 12:31:07 +0800

    index penduduk dan kartu keluarga

---
+++ src/views/dashboard/kependudukan/kartu-keluarga/Edit.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container
+    id="kk"
+    fluid
+    tag="section"
+  >
+    <base-material-card
+      icon="mdi-file"
+      class="px-5 py-3"
+      title="Input Kartu Keluarga"
+    >
+      <v-col sm="6">
+        <v-row>
+          <v-text-field
+            label="No. Kartu Keluarga"
+          />
+          <v-btn
+            class="mr-4"
+            color="warning"
+            @click="submit"
+          >
+            Cek
+          </v-btn>
+        </v-row>
+        <v-row>
+          <v-text-field
+            label="Cari Anggota Keluarga"
+          />
+          <v-btn
+            class="mr-4"
+            color="success"
+            @click="submit"
+          >
+            Cek
+          </v-btn>
+        </v-row>
+      </v-col>
+      <v-btn
+        class="mr-4"
+        color="blue"
+        @click="submit"
+      >
+        Simpan
+      </v-btn>
+      <v-btn
+        color="error"
+        @click="clear"
+      >
+        clear
+      </v-btn>
+    </base-material-card>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'DashboardPenduduk',
+
+    data: () => ({
+    }),
+    submit () {
+      this.$v.$touch()
+    },
+    clear () {
+      this.$v.$reset()
+      this.name = ''
+      this.email = ''
+      this.select = null
+      this.checkbox = false
+    },
+  }
+</script>
